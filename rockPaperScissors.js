11a9cfbdbd5a30053f8b46e736f212b17d9982e9
Eric Kumasaka <emkuma@gmail.com>
2026-04-18 11:25:51 -0500

    Rough draft

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let tally = [];
 
 function getComputerChoice() {
   randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -22,29 +23,45 @@ function getComputerChoice() {
   return humanChoice;
  }
 
- function playRound(humanChoice, computerChoice) {
+ function playRound(humanChoice, computerChoice, humanScore, computerScore) {
   if (humanChoice === computerChoice) {
     console.log(`${humanChoice} ties with ${computerChoice}!`)
+    return;
   }
 
   if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock") {
     humanScore ++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    console.log(`Your score: ${humanScore}   Computer: ${computerScore}`)
+    winner = 'You';
   }
   if (humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
     computerScore ++;
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    console.log(`Your score: ${humanScore}   Computer: ${computerScore}`)
+    winner = 'Computer';
   }
   if (humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "rock" && computerChoice === "scissors") {
     humanScore ++;
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    console.log(`Your score: ${humanScore}   Computer: ${computerScore}`)
+    winner = 'You';
   }
-  
+  tally = [winner, humanScore, computerScore];
+
+  return tally;
+ }
+
+ function playGame(humanChoice, computerChoice, humanScore, computerScore) {
+  for (let i = 0; i < 5; i++) {
+    playRound(humanChoice, computerChoice, humanScore, computerScore);
+
+    if (tally[0] == 'You') {
+      console.log(`${tally[0]} win!  ${humanChoice} beats ${computerChoice}`);
+    }  
+    else if (tally[0] == 'Computer') {
+      console.log(`${tally[0]} wins!  ${computerChoice} beats ${humanChoice}!`);
+    }
+    console.log(`You: ${tally[1]}  Computer: ${tally[2]}`); 
+  }
+ }
+
  getComputerChoice();
 
  getHumanChoice();
 
- playRound(humanChoice, computerChoice);
+ playGame(humanChoice, computerChoice, humanScore, computerScore, tally);
